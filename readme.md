f34eef1e57f9dc98875b505737366d934e14d182
Gem Newman <spurll@gmail.com>
2020-01-17 14:13:59 -0600

    Fix errors related to API changes at TheTVDB

--- readme.md
+++ readme.md
@@ -1,6 +1,12 @@
 # Overview
 
-## season_information.py
+This is pretty much the first thing I ever wrote in Python, so... it's not great!
+
+## Setup
+
+Make sure you have an (API key for TheTVDB.com](https://thetvdb.com/api-information), then create a file called `apikey` containing only that key.
+
+## season\_information.py
 
 Fetches season and episode information for a television show from TheTVDB.com.
 
@@ -12,7 +18,7 @@ Positional:
 Flags:
 * -v, --dvd: List episodes by DVD order instead of aired order (if available).
 
-## label_episodes.py
+## label\_episodes.py
 
 Renames media files in a directory to correspond to the episode list of a television show. (Useful when you want to correctly label episodes from ripped DVDs for use in Plex, for example.) Episode information is scraped from TheTVDB.com, via `season_information.py`.
 
@@ -74,7 +80,7 @@ Options:
 ## Known Bugs
 
 * UTF-8 encoded unicode (common with TheTVDB.com) messes with the character count in tables displayed by `season_information.py`.
-* It looks like season_information.py crashes when TheTVDB.com returns no search results. That's an embarassingly trivial error.
+* It looks like season\_information.py crashes when TheTVDB.com returns no search results. That's an embarassingly trivial error.
 
 # TheTVDB.com
 
--- season_information.py
+++ season_information.py
@@ -3,16 +3,16 @@
 # Written by Gem Newman. This work is licensed under a Creative Commons
 # Attribution-NonCommercial-ShareAlike 3.0 Unported License.
 
-# For more information about TheTVDB.com's API, visit
-# http://thetvdb.com/wiki/index.php?title=Programmers_API
 
-# TO DO:
-#   Fix bug in table.py where UTF-8 encoded unicode messes with the character
-#     count.
-
-import argparse, urllib2, datetime, random, re, xml.etree.ElementTree as et
+import argparse, requests, datetime, random, re, os
+import xml.etree.ElementTree as et
 from table import table, menu
 
+
+KEYPATH = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'apikey')
+
+with open(KEYPATH, 'r') as file:
+    KEY = file.read().replace('\n', '')
 
 DATE_FORMAT = "{:%d %B %Y}"     # For viewing air date in season information.
 DESCRIPTION_LIMIT = 80          # Number of characters to display.
@@ -25,36 +25,21 @@ EPISODE_NAME = "EpisodeName"    # XML identifier for episode name.
 AIR_DATE = "FirstAired"         # XML identifier for air date.
 DESCRIPTION = "Overview"        # XML identifier for episode description.
 
-DATE_CONVERSION = lambda d: DATE_FORMAT.format(datetime.datetime.strptime(d,
-                                               "%Y-%m-%d"))
-
-KEY = "F6C8EF890E843081"
-MIRROR_URL = "http://thetvdb.com/api/{key}/mirrors.xml"
-SERIES_URL = '{mirror}/api/GetSeries.php?seriesname="{name}"'
-EPISODE_URL = "{mirror}/api/{key}/series/{series_id}/all/en.xml"
+SERIES_URL = 'https://thetvdb.com/api/GetSeries.php?seriesname="{name}"'
+EPISODE_URL = "https://thetvdb.com/api/{key}/series/{series_id}/all/en.xml"
 
 
 def season_information(series, dvd):
-    # Randomly selects a mirror to connect to.
-    response = urllib2.urlopen(MIRROR_URL.format(key=KEY))
-    xml = response.read()
-    root = et.fromstring(xml)
-
-    mirrors = root.findall("Mirror")
-    if len(mirrors) < 1:
-        print "Warning: No mirrors found. Failing over to TheTVDB.com..."
-        mirror = "http://thetvdb.com"
-    else:
-        mirror = grab(mirrors[random.randint(0, len(mirrors) - 1)],
-                      "mirrorpath")
-        print "Mirror: {}".format(mirror)
-
     # Identify the series by name, and retrieve its ID from TheTVDB.com.
     print "Search: {}".format(series)
 
     series = series.replace(" ", "%20")
-    response = urllib2.urlopen(SERIES_URL.format(mirror=mirror, name=series))
-    xml = response.read()
+    header = {"User-Agent": "Season Information"}
+
+    # Retrieve series information.
+    response = requests.get(SERIES_URL.format(name=series), headers=header)
+
+    xml = response.text.encode("utf-8")
     root = et.fromstring(xml)
 
     series = root.findall("Series")
@@ -83,9 +68,10 @@ def season_information(series, dvd):
     print "Series ID: {}".format(series_id)
 
     # Retrieve the episode list for the series.
-    response = urllib2.urlopen(EPISODE_URL.format(mirror=mirror, key=KEY,
-                               series_id=series_id))
-    xml = response.read()
+    response = requests.get(EPISODE_URL.format(key=KEY, series_id=series_id),
+        headers=header)
+
+    xml = response.text.encode("utf-8")
     root = et.fromstring(xml)
 
     episodes = [{"season": grab(e, SEASON, int),
@@ -93,7 +79,8 @@ def season_information(series, dvd):
                 "dvd_season": grab(e, DVD_S, int),
                 "dvd_episode": grab(e, DVD_E, int),
                 "name": grab(e, EPISODE_NAME),
-                "date": grab(e, AIR_DATE, DATE_CONVERSION),
+                "date": grab(e, AIR_DATE, lambda d: DATE_FORMAT.format(
+                    datetime.datetime.strptime(d, "%Y-%m-%d"))),
                 "description": grab(e, DESCRIPTION)}
                 for e in root.findall("Episode")]
 
@@ -135,7 +122,6 @@ def season_information(series, dvd):
             s = int(s)
             rows = [(e["episode"], e["name"], e["date"],
                     short_description(e["description"])) for e in episodes[s]]
-            #print rows
             menu("Season {}".format(s), *zip(*rows), headers=["Episode",
                  "Name", "Air Date", "Description"],
                  footer="ENTER to continue.")
